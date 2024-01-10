c3d3224577e6940af8553f6cc200cbf950e83763
yanovitchsky <yannakoun@gmail.com>
2024-01-10 17:09:30 +0000

    Force raise if decode is empty

--- lib/prefixed_ids/prefix_id.rb
+++ lib/prefixed_ids/prefix_id.rb
@@ -19,7 +19,8 @@ module PrefixedIds
     def decode(id, fallback: false)
       fallback_value = fallback ? id : nil
       _, id_without_prefix = PrefixedIds.split_id(id, @delimiter)
-      decoded_hashid = @hashids.decode_hex(id_without_prefix) rescue id
+      # decoded_hashid = @hashids.decode_hex(id_without_prefix) rescue id
+      decoded_hashid = decode_hex(@hashids, id_without_prefix) rescue id
       if fallback && !valid?(decoded_hashid, id)
         fallback_value
       else
@@ -35,6 +36,12 @@ module PrefixedIds
 
     def tranform_to_uuid(hex_string)
       "#{hex_string[0,8]}-#{hex_string[8,4]}-#{hex_string[12,4]}-#{hex_string[16,4]}-#{hex_string[20,12]}"
+    end
+
+    def decode_hex(hash, str)
+      res = hash.decode_hex(str)
+      raise Hashids::InputError if res.empty?
+      res
     end
   end
 end
